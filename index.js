d2f69128ddb406200005db5a08d1c1ef2e396143
t3rr0r <mail@t3rr0r.com>
2018-06-02 15:25:01 -0400

    added user agent header + added package-lock.json file

--- index.js
+++ index.js
@@ -4,38 +4,41 @@ var cheerio = require('cheerio');
 var states = require('./states');
 
 module.exports = function(opts, cb) {
-    request({
-	url: 'https://nominatim.openstreetmap.org/',
-	json: true,
-	qs: {
-	    format: 'json',
-	    addressdetails: 1,
-	    q: opts.address
-	}
-    }, function(err, res, data) {
-	if (err) {
-	    cb(err);
-	    return;
-	}
+  request({
+    url: 'https://nominatim.openstreetmap.org/',
+    json: true,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    },
+    qs: {
+      format: 'json',
+      addressdetails: 1,
+      q: opts.address
+    }
+  }, function(err, res, data) {
+    if (err) {
+      cb(err);
+      return;
+    }
 
-	if (!data.length) {
-	    cb(new Error('unable to find address'));
-	    return;
-	}
+    if (!data.length) {
+      cb(new Error('unable to find address'));
+      return;
+    }
 
-	var item = data[0];
+    var item = data[0];
 
-	if (!item.address.state) {
-	    cb(new Error('could not determine state'));
-	    return;
-	}
+    if (!item.address.state) {
+      cb(new Error('could not determine state'));
+      return;
+    }
 
-        opts.dob = moment(opts.dob, 'MM-DD-YYYY');
-	opts.address = item.address;
-	opts.address.county = opts.address.county && opts.address.county.replace('County', '').trim();
+    opts.dob = moment(opts.dob, 'MM-DD-YYYY');
+    opts.address = item.address;
+    opts.address.county = opts.address.county && opts.address.county.replace('County', '').trim();
 
-	var state = states[opts.address.state.toLowerCase()];
+    var state = states[opts.address.state.toLowerCase()];
 
-	state(opts, cb);
-    });
+    state(opts, cb);
+  });
 };
